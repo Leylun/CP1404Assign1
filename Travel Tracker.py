a44661f42276b8f6eca7e1b418f4ebb278f3ac1a
Tristan <lordtharr@gmail.com>
2020-09-01 13:20:29 +1000

    Arranged list given from text file Added function menu choice features (displaying list, creating new entry) Added new functions for future handling, type correction and displaying list Added Comments to assist code readers.

--- Travel Tracker.py
+++ Travel Tracker.py
@@ -6,23 +6,58 @@ def main():
         list_places = list(csv.reader(file_places))
     print("Travel Tracker 1.0 - by Tristan Harrington")
     print(len(list_places), "places loaded from places.csv")
+    arranged_list = list_places
+    for place in arranged_list:
+        if place[3] == "v":
+            place[3] = "*"
+        else:
+            place[3] = ""
+    arranged_list.sort()
+    # print(arranged_list)
+    display_list(arranged_list)
+    program_continue = True
+    while program_continue:
+        print("L - List places", "A - Add new place", "M - Mark place as visited", "Q - Quit", sep="\n")
+        # TODO: Not correct handling just placeholder --
+        user_choice = input(">>")
+        program_continue = menu(arranged_list, user_choice)
 
 
-def menu(user_choice, places):
+def menu(places, user_choice):
+    new_location = []
     menu_choice = user_choice.upper()
-    print("L - List places", "A - Add new place", "M - Mark place as visited", "Q - Quit", sep="\n")
     if menu_choice == "L":
-        # display list of places (arranged)
-        print(places)
+        # Comment// Prints places; in ascending order. (relative to priority with places visited as top)
+        display_list(places)
     elif menu_choice == "A":
-        # add a new place
-        name = input("Name:")
+        # Comment// Asks for a new location, appends to a list, which is added to the master list.
+        # TODO: This is just a placeholder not correct handling
+        new_location.append(input("Name: "))
+        new_location.append(input("Country: "))
+        new_location.append(input("Priority: "))
+        new_location.append("")
+        places.append(new_location)
     elif menu_choice == "M":
-        # mark a place as visited
-        print(places)
+        # Comment//
+        display_list(places)
     elif menu_choice == "Q":
         # say places saved
         print("Have a nice day :)")
+        return True
+    else:
+        # TODO: Not correct handling; just placeholder --
+        print("Incorrect Response: Please Try Again")
+    return False
 
+
+def type_check(given_type):
+    # TODO: Finish this during last update to help with user handling
+    return given_type
+
+
+def display_list(given_list):
+    # TODO: This is just a placeholder and not correct handling
+    for index in given_list:
+        print("{} {} {} {}".format(index[3], index[0], index[1], index[2]))
 
 main()
